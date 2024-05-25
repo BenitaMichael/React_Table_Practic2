506857ceaee2c45488ac66b9fb102ef4fa051187
BenitaMichael <benubiaka@gmail.com>
2024-05-26 00:14:11 +0100

    feat: table definition

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { useTable } from 'react-table'
+import DATA from './data.json'
+import COLUMNS from './columns'
 
 const Main = () => {
-  return (
-    <div className='main'>
-        
-    </div>
-  )
+
+    const columns = useMemo(() => COLUMNS, []);
+    const data = useMemo(() => DATA, [])
+
+    const tableInstance = useTable({
+        columns,
+        data
+    })
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        prepareRow,
+        rows,
+        headerGroups
+    } = tableInstance
+
+    return (
+        <div className='main'>
+            <table {...getTableProps()}>
+                <thead>
+                {
+                    headerGroups.map((headerGroup) =>(
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {
+                                headerGroup.headers.map(colum => (
+                                    <th {...columns.getHeaderProps()}>{columns.render('Header')}</th>
+                                ))
+                            }
+                        </tr>
+                    ))
+                }
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {
+                        rows.map(row => {
+                            prepareRow(row)
+                            return(
+                                <tr {...row.getRowProps()}>
+                                    {row.cells.map((cell) => {
+                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                    })}
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Main
