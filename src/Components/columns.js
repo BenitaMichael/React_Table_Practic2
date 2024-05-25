3f4fb3a1df7c02066420a4d0d5efddfdea0258ce
BenitaMichael <benubiaka@gmail.com>
2024-05-25 23:21:24 +0100

    feat: column definition

---
+++ src/Components/columns.js
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+    {
+        Header: "ID",
+        accessor: "plugID"
+    },
+    {
+        Header: "Plugin Name",
+        accessor: "plugin_name"
+    },
+    {
+        Header: "Location",
+        accessor: "plugin_location"
+    },
+    {
+        Header: "Status",
+        accessor: "plugin_stats"
+    },
+    {
+        Header: "Bounce Rate",
+        accessor: "plugin_rate"
+    }
+]
