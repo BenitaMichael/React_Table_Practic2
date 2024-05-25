18aa254631d029a3ed9a794df7f6951b0c59d565
BenitaMichael <benubiaka@gmail.com>
2024-05-25 21:23:22 +0100

    feat: footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 import React from 'react'
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <>
         <Header />
+        <Footer />
     </>
   )
 }
