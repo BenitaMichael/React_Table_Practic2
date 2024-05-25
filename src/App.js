b27f47bb9811e77dafd47a0123d6ef3f0565e869
BenitaMichael <benubiaka@gmail.com>
2024-05-25 21:14:38 +0100

    feat: header component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
 import './App.css';
+import Header from './Components/Header';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+        <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Bet Scrapper</h1>
+    </div>
+  )
+}
+
+export default Header
